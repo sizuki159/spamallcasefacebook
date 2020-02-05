bb68e8f45f0b4bd57520f7a28499426622221ec1
sizuki159 <luahantinhthu9@gmail.com>
2020-02-05 13:26:15 +0700

    Add project files.

---
+++ CaseFacebook/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace CaseFacebook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown_numberspam = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown_delay = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_numberspam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_delay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Spam Nào";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID CASE";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // numericUpDown_numberspam
+            // 
+            this.numericUpDown_numberspam.Location = new System.Drawing.Point(124, 61);
+            this.numericUpDown_numberspam.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown_numberspam.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_numberspam.Name = "numericUpDown_numberspam";
+            this.numericUpDown_numberspam.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown_numberspam.TabIndex = 4;
+            this.numericUpDown_numberspam.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Số Lần Spam";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Delay (Seconds)";
+            // 
+            // numericUpDown_delay
+            // 
+            this.numericUpDown_delay.Location = new System.Drawing.Point(124, 100);
+            this.numericUpDown_delay.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown_delay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown_delay.Name = "numericUpDown_delay";
+            this.numericUpDown_delay.Size = new System.Drawing.Size(98, 20);
+            this.numericUpDown_delay.TabIndex = 7;
+            this.numericUpDown_delay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 166);
+            this.Controls.Add(this.numericUpDown_delay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown_numberspam);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Spam All Case Facebook";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_numberspam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_delay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown_numberspam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown_delay;
+    }
+}
+
---
+++ CaseFacebook/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xNet;
+
+namespace CaseFacebook
+{
+    public partial class Form1 : Form
+    {
+        string path_img = String.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown_numberspam.Value = 1;
+            numericUpDown_delay.Value = 10;   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            path_img = ChooseFile();
+            if (path_img == String.Empty)
+            {
+                MessageBox.Show("Không Tìm Thấy File Ảnh");
+                this.Close();
+            }
+            else
+            {
+                ThreadStart start_play = new ThreadStart(start);
+                Thread Begin = new Thread(start_play);
+                Begin.Start();
+            }
+        }
+
+        void start()
+        {
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown_numberspam.Value); i++)
+            {
+                UploadFile(path_img);
+                Thread.Sleep(Convert.ToInt32(numericUpDown_delay.Value) * 1000);
+            }
+            MessageBox.Show("Done!");
+        }
+
+        string UploadData(HttpRequest http, string url, MultipartContent data = null, string contentType = null, string userArgent = null)
+        {
+            if (http == null)
+            {
+                http = new HttpRequest();
+            }
+
+            if (!string.IsNullOrEmpty(userArgent))
+            {
+                http.UserAgent = userArgent;
+            }
+
+            string html = http.Post(url, data).ToString();
+            return html;
+        }
+
+        void UploadFile(string path)
+        {
+            MultipartContent data = new MultipartContent() {
+                { new FileContent(path), "attach", Path.GetFileName(path)}
+            };
+
+            UploadData(null, "https://www.facebook.com/support/support_case/do_reply/?eid=" + textBox1.Text, data, null, "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0");
+        }
+
+        string ChooseFile()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                return dialog.FileName;
+            }
+            return String.Empty;
+        }
+    }
+}
